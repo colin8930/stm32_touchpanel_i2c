578035566d29f1406927644d705257fe23562ffa
colin <colin8930@gmail.com>
2014-11-28 19:49:22 +0800

    upload the code

---
+++ CORTEX_M4F/main.c
@@ -0,0 +1,109 @@
+/**
+  ******************************************************************************
+  * @file    Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    20-September-2013
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/** @addtogroup Template
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+TP_STATE* tp;
+
+void
+prvInit()
+{
+	//LCD init
+	LCD_Init();
+	IOE_Config();
+	LTDC_Cmd( ENABLE );
+
+	LCD_LayerInit();
+	LCD_SetLayer( LCD_FOREGROUND_LAYER );
+	LCD_Clear( LCD_COLOR_BLACK );
+	LCD_SetTextColor( LCD_COLOR_WHITE );
+
+	
+}
+
+static char* itoa(int value, char* result, int base)
+{
+	if (base < 2 || base > 36) {
+		*result = '\0';
+		return result;
+	}
+	char *ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+	} while (value);
+
+	if (tmp_value < 0) *ptr++ = '-';
+	*ptr-- = '\0';
+	while (ptr1 < ptr) {
+		tmp_char = *ptr;
+		*ptr-- = *ptr1;
+		*ptr1++ = tmp_char;
+	}
+	return result;
+}
+
+//Main Function
+int main(void)
+{
+	prvInit();
+	while(1){
+
+		if( (tp=IOE_TP_GetState())->TouchDetected ){
+
+			LCD_Clear(LCD_COLOR_BLACK);			
+			LCD_SetTextColor( LCD_COLOR_BLACK );
+			char str1[16] = "X: ";
+			itoa((int)tp->X, str1 + 3, 10);
+			char str2[16] = "Y: ";
+			itoa((int)tp->Y, str2 + 3, 10);
+			LCD_DisplayStringLine(LCD_LINE_6, str1);
+			LCD_DisplayStringLine(LCD_LINE_7, str2);
+
+		}
+
+
+
+
+	}
+	
+}
